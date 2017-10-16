a91da4ed2841df751a10b4d6d561b45b406516e5
Nathaniel Weeks <Nate@Nathaniels-MacBook-Pro.local>
2017-10-16 16:49:37 -0400

    devise user authentication

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "user signs up" do
+    it { should have_valid(:email).when('123@gmail.com') }
+    it { should_not have_valid(:email).when(nil) }
+
+  end
+
+    it 'has a matching password confirmation for the password' do
+      user = User.new
+      user.password = 'password'
+      user.password_confirmation = 'anotherpassword'
+      binding.pry
+
+      expect(user).to_not be_valid
+      expect(user.errors[:password_confirmation]).to_not be_blank
+
+
+    end
+end
