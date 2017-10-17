65de4917b426c0e7b12b10b442578282f7ba8184
Luigi Lake <lfdlake@gmail.com>
2017-10-17 14:02:30 -0400

    add model tests for celestials and reviews

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe Review do
+  it { should belong_to :user }
+  it { should belong_to :celestial }
+
+  it { should have_valid(:body).when('hahahahahaha') }
+  it { should_not have_valid(:body).when(nil, '') }
+
+  it { should have_valid(:votes).when(1)}
+  it { should_not have_valid(:votes).when(nil, '')}
+  it { should_not have_valid(:votes).when('hahahahaha')}
+
+  it { should have_valid(:rating).when(5)}
+  it { should_not have_valid(:rating).when(nil, '')}
+  it { should_not have_valid(:rating).when(20)}
+end
