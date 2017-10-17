ebdb0c6d19e326eed2ca19dcd475ec7fe547d4cc
Luigi Lake <lfdlake@gmail.com>
2017-10-17 13:16:31 -0400

    create tests for celestials controller

---
+++ spec/controllers/api/v1/celestials_controller_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::CelestialsController, type: :controller do
+  let!(:user) {User.create(email: "123@gmail.com", password: "1234567", password_confirmation: "1234567")}
+  let!(:planet1) {Celestial.create(name: "Planet1", distance: 1, celestial_type: "planet", size: 2, user: user)}
+
+  describe "Get#index" do
+    before do
+      get :index
+    end
+
+    it "should return a json" do
+      returned_json = JSON.parse(response.body)
+      expect(response.status).to eq 200
+    end
+
+    it "should return a json with a name, distance, celestial_type, and size" do
+      returned_json = JSON.parse(response.body)
+      expect(returned_json[0]["name"]).to eq "Planet1"
+      expect(returned_json[0]["distance"]).to eq 1
+      expect(returned_json[0]["celestial_type"]).to eq "planet"
+      expect(returned_json[0]["size"]).to eq 2
+    end
+
+  end
+end
