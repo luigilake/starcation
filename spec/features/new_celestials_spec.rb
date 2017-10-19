0536b0abc2b3b0bc83315911f2d2be2be1cd41ec
Joe Musacchia <jmusacchia@joesmac-2.local>
2017-10-19 14:47:18 -0400

    added tests for new celestial form

--- spec/features/new_celestials_spec.rb
+++ spec/features/new_celestials_spec.rb
@@ -25,6 +25,34 @@ feature 'add new celestial' do
 
   end
 
+  scenario 'specifying valid and required information' do
+    visit new_user_session_path
+    click_link 'Sign up'
+    fill_in 'Username', with: 'testname'
+    fill_in 'First name', with: 'Jon'
+    fill_in 'Last name', with: 'Smith'
+    fill_in 'Email', with: '123@gmail.com'
+    fill_in 'Password', with: '1234567'
+    fill_in 'Password confirmation', with: '1234567'
+    click_button 'Sign up'
+
+    visit new_celestial_path
+    fill_in 'Name', with: ''
+    fill_in 'Distance', with: ''
+    select 'Planet', from: 'Celestial type'
+    fill_in 'Size', with: ''
+    click_button 'Add Celestial'
+
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Distance can't be blank")
+    expect(page).to have_content("Size can't be blank")
+
+  end
+
+
+
+
+
   # scenario '' do
   #
   # end
