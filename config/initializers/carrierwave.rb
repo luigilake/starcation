33f56ea51a854b76038873a0e023fb773162000c
joemusacchia <jmusacchia@joesmac-2.local>
2017-10-20 10:09:04 -0400

    fixed s3 problem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,7 +16,8 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
     :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],     # required
+    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]     # required
+    # :region                 => ENV['AWS_REGION']                        # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET']              # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
